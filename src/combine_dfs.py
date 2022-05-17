94ea5535f5a2b8bd9807a45d589f6eed5f0696e1
SaraMøllerØstergaard#1413 <sara.m.oestergaard@gmail.com>
2022-05-17 09:58:40 +0200

    combine with new change points

--- src/combine_dfs.py
+++ src/combine_dfs.py
@@ -10,7 +10,7 @@ import numpy as np
 
 if __name__ =="__main__":
     ### Loading information dynamics and raw emotions
-    dynamics_model_path = os.path.join("..", "data", "idmdl", "tweets_emo_date_W3.csv")
+    dynamics_model_path = os.path.join("data", "idmdl", "tweets_emo_date_W3.csv")
     dynamics_df = pd.read_csv(dynamics_model_path)
 
     labels = [
@@ -27,7 +27,7 @@ if __name__ =="__main__":
     dynamics_df_w_change = changepoint.write_model_df(dynamics_df, change_locations, labels=labels)
 
     ### Loading SSI timeline
-    timeline_path = os.path.join("..", "data", "covid_events", "timeline_covid.xlsx")
+    timeline_path = os.path.join("data", "covid_events", "timeline_covid.xlsx")
     timeline = pd.read_excel(open(timeline_path, "rb"), engine='openpyxl')
     timeline = covid_clean.clean_timeline(timeline)
     timeline = covid_clean.get_dummies(timeline, cols_to_dummy=["type", "nationality"])
@@ -38,7 +38,7 @@ if __name__ =="__main__":
         dynamics_timeline[col] = [0 if np.isnan(x) else int(x) for x in dynamics_timeline[col]]
 
     ### Loading n cases
-    owid_path = os.path.join("..", "data", "covid_events", "owid-covid-data.csv")
+    owid_path = os.path.join("data", "covid_events", "owid-covid-data.csv")
     owid = pd.read_csv(owid_path)
     owid = owid[owid['location']=='Denmark']
     owid['new_cases_MA7'] = owid['new_cases'].rolling(window=7).mean()
@@ -46,5 +46,5 @@ if __name__ =="__main__":
     ## Combing woid with dynamics and timeline
     global_df = dynamics_timeline.merge(owid[["date","new_cases", "new_cases_MA7"]], how="left", on="date")
     
-    out_path = os.path.join("..", "data", "model_df.csv")
-    # global_df.to_csv(out_path)
+    out_path = os.path.join("data", "model_df.csv")
+    global_df.to_csv(out_path)
